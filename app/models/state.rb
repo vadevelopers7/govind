ab38ff83258fa44f5ead2f1cf280ae74b73eb0aa
Vishal Nagda <vishalnagda1@gmail.com>
2017-04-02 15:09:57 +0530

    [#4] added city has many states validation spec in state model

--- app/models/state.rb
+++ app/models/state.rb
@@ -1,5 +1,6 @@
 class State < ActiveRecord::Base
   belongs_to :country
+  has_many :cities
   validates_presence_of :country_id, :name, :code
   validates_uniqueness_of :name, :code, case_sensitive: false, scope: :country_id, message: "already exist."
 end
--- spec/models/state_spec.rb
+++ spec/models/state_spec.rb
@@ -9,12 +9,15 @@ RSpec.describe State, type: :model do
     end
   end
 
+  before(:each) do
+    @country = FactoryGirl.create(:country)
+  end
+
   describe "belongs_to" do
   	context "country" do
   		it "should return country of the current state" do
-  			country = FactoryGirl.create(:country)
-  			state = FactoryGirl.create(:state, country_id: country.id)
-  			expect(state.country).to eq(country)
+  			state = FactoryGirl.create(:state, country_id: @country.id)
+  			expect(state.country).to eq(@country)
   		end
   	end
   end
@@ -22,23 +25,29 @@ RSpec.describe State, type: :model do
   describe "validates_uniqueness_of" do
     context "name" do
       it "should not valid if same state the in same country" do
-        @country = FactoryGirl.create(:country)
         expect(FactoryGirl.create(:state, country_id: @country.id, name: "Rajasthan")).to be_valid
         expect(FactoryGirl.build(:state, country_id: @country.id, name: "rajasthan")).not_to be_valid
       end
       it "should allow to create same state name in different country." do
-        country = FactoryGirl.create(:country)
-        expect(FactoryGirl.create(:state, country_id: country.id)).to be_valid
+        expect(FactoryGirl.create(:state, country_id: @country.id)).to be_valid
       end
     end
 
     context "code" do
       it "should not allow to create same state code the in same country" do
-        country = FactoryGirl.create(:country)
-        expect(FactoryGirl.create(:state, country_id: country.id, name: "Rajasthan")).to be_valid
-        expect(FactoryGirl.build(:state, country_id: country.id, name: "Gujrat")).not_to be_valid
+        expect(FactoryGirl.create(:state, country_id: @country.id, name: "Rajasthan")).to be_valid
+        expect(FactoryGirl.build(:state, country_id: @country.id, name: "Gujrat")).not_to be_valid
       end
     end
 
+  describe "has_many" do
+    context "cities" do
+      it "should return cities of current state" do
+        state = FactoryGirl.create(:state, country_id: @country.id)
+        city = FactoryGirl.create(:city, state_id: state.id)
+        city1 = FactoryGirl.create(:city, state_id: state.id, name: "Jaipur")
+        expect(state.cities).to eq([city, city1])
+      end
+    end
   end
 end
