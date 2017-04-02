60150b9096975479beca3f860862577a3d90dad2
Vishal Nagda <vishalnagda1@gmail.com>
2017-04-02 14:11:34 +0530

    [#5] update rspec configuration file.

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -61,7 +61,7 @@ RSpec.configure do |config|
   config.after(:each) do
     DatabaseCleaner.clean
   end
-  
+
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
   # examples within a transaction, remove the following line or assign false
   # instead of true.
