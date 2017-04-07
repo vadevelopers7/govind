eb93ab1edc6a636d752510b68db85e235349223f
Vishal Nagda <vishalnagda1@gmail.com>
2017-04-07 20:58:07 +0530

    [#142907081] update state model.

--- app/models/state.rb
+++ app/models/state.rb
@@ -3,5 +3,5 @@ class State < ActiveRecord::Base
   has_many :cities
   validates_presence_of :country_id, :name, :code
   validates_uniqueness_of :name, :code, case_sensitive: false, scope: :country_id, message: "already exist."
-  scope :of_country, -> (id) { where country_id: id }
+  scope :of_country, -> (country_id) { where country_id: country_id }
 end
