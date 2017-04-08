e760382e821ce9a1d37e8cc4ce2897689bf0a544
Vishal Nagda <vishalnagda1@gmail.com>
2017-04-08 18:55:41 +0530

    [#143363453] update category model defaule scope to render categories order by sequence_id.

--- app/models/category.rb
+++ app/models/category.rb
@@ -3,6 +3,6 @@ class Category < ActiveRecord::Base
   validates_presence_of :city_id, :sequence_id, :name, :icon
   validates_numericality_of :city_id, :sequence_id
   validates_uniqueness_of :name, :sequence_id, case_sensitive: false, scope: :city_id, message: "already exist."
-  default_scope { order(:created_at) }
+  default_scope { order(:sequence_id) }
   scope :of_city, -> (city_id) { where city_id: city_id }
 end
