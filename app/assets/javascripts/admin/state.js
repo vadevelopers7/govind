8b27171d7bcf87e9bc30d17ddadf2493ccda1591
Ajay Tanwar <ajaytanwar@Ajays-MacBook-Air.local>
2017-04-08 17:25:18 +0530

    [#143362745] submit state detail

--- app/assets/javascripts/admin/state.js
+++ app/assets/javascripts/admin/state.js
@@ -6,6 +6,7 @@ LD.State = function () {
 LD.State.prototype= {
   initialize:function() {
   	this.loadCountryData();
+  	this.addedState();
   },
 
   loadCountryData: function() {
@@ -25,5 +26,49 @@ LD.State.prototype= {
 	     
 	    }
 	  });
+  },
+
+  addedState: function() {
+  	$('#stateContainer #stateForm #submitState').unbind('click');
+  	$('#stateContainer #stateForm #submitState').on('click', function(e) {
+  		e.preventDefault();
+  		params = {};
+  		var active = "";
+  		params['name'] = $('#stateContainer #stateForm #title').val();
+  		params['code'] = $('#stateContainer #stateForm #code').val().toUpperCase();
+  		params['country_id'] = $('#stateContainer #stateForm #selectCountry').val().toUpperCase();
+
+  		if ($('#stateContainer #stateForm #status').is(':checked')) {
+  			active = true;
+  		}
+  		else
+  		{
+  			active = false;
+  		}
+  		params['active'] = active;
+  		if($("#stateContainer #stateForm").valid()) {
+	  		$.ajax({
+	        url: '/admin/states/',
+	        type: 'POST',
+	        data: {state: params},
+	        format: 'JSON',
+	        success: function (data, textStatus, jqXHR) {
+	        	var stateData = data.state;
+	          $('#stateContainer #stateTable').bootstrapTable('insertRow', {index: 0, row: {
+	              name : stateData.name,
+	              code : stateData.code,
+	              country_name : stateData.country_id,
+	              active : stateData.active
+            	}
+          	});
+
+            $('#stateContainer #stateModal').modal("hide");
+	        },
+	        error: function (jqXHR, textStatus, errorThrown) {
+	         
+	        }
+	      });
+	    }
+  	});
   }
 }
