2cedb6ccda1144adc379f6cbedf8fa5002f702ae
Ajay Tanwar <ajaytanwar@Ajays-MacBook-Air.local>
2017-04-08 13:46:28 +0530

    [#143359993] validation added for country form

--- app/assets/javascripts/admin/country.js
+++ app/assets/javascripts/admin/country.js
@@ -6,6 +6,7 @@ LD.Country = function () {
 LD.Country.prototype= {
   initialize:function() {
   	this.resetCountryForm();
+  	this.formValidation();
     this.addedCountry();
   },
 
@@ -18,6 +19,19 @@ LD.Country.prototype= {
   	});
   },
 
+ 	formValidation: function() {
+ 		$("#countryContainer #countryForm").validate({
+      rules: {
+        country_title: "required",
+        country_code: "required"
+      },
+      messages: {
+        country_title: "Please enter Country Name",
+        country_code: "Please enter Country Code"
+      }
+    });
+  },
+
   addedCountry: function() {
   	$('#countryContainer #countryForm #submitCountry').unbind('click');
   	$('#countryContainer #countryForm #submitCountry').on('click', function(e) {
@@ -26,20 +40,20 @@ LD.Country.prototype= {
   		params['name'] = $('#countryContainer #countryForm #title').val();
   		params['code'] = $('#countryContainer #countryForm #code').val().toUpperCase();
   		params['active'] = $('#countryContainer #countryForm #status').val();
-
-  		$.ajax({
-        url: '/admin/countries/',
-        type: 'POST',
-        data: {country: params},
-        format: 'JSON',
-        success: function (data, textStatus, jqXHR) {
-          console.log(data);
-        },
-        error: function (jqXHR, textStatus, errorThrown) {
-         
-        }
-      });
-
+  		if($("#countryContainer #countryForm").valid()) {
+	  		$.ajax({
+	        url: '/admin/countries/',
+	        type: 'POST',
+	        data: {country: params},
+	        format: 'JSON',
+	        success: function (data, textStatus, jqXHR) {
+	          console.log(data);
+	        },
+	        error: function (jqXHR, textStatus, errorThrown) {
+	         
+	        }
+	      });
+	    }
   	});
   }
 }
