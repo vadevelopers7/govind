0e603d6641f1de3eb1b19b67df9ccd27990b9528
Vishal Nagda <vishalnagda1@gmail.com>
2017-04-24 23:35:43 +0530

    [#143774621] update user model.

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,23 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  validates_inclusion_of :role, in: ['shopper', 'retailer', 'admin']
+  after_create :send_welcome_email
+
+  def is_admin?
+    self.role == "admin"
+  end
+  def is_shopper?
+    self.role == "shopper"
+  end
+  def is_retailer?
+    self.role == "retailer"
+  end
+
+  private
+  def send_welcome_email
+    mailer = LoveyDovyMailer.send_signup_email(self)
+    mailer.deliver_now
+  end
 end
