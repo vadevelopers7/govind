ed70d0af7a55b5c2bafd81b677662b77f95b2c28
Ajay Tanwar <ajaytanwar@Ajays-MacBook-Air.local>
2017-06-03 11:41:09 +0530

    Remove bootstrap table & Added Datatable css & js

--- app/assets/javascripts/common_dashboard_manifest.js
+++ app/assets/javascripts/common_dashboard_manifest.js
@@ -1,6 +1,6 @@
 //= require common/jquery.min
 //= require common/custom.min
 //= require common/bootstrap.min
-//= require common/bootstrap-table.js
 //= require common/jquery.validate.min
 //= require common/icheck.min
+//= require common/table/jquery.dataTables.min
